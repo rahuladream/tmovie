6a13d1dcd954516857fcebd4f7da9f2728f76a77
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-20 01:23:11 +0530

    User registration api logic added

--- app/movie/views.py
+++ app/movie/views.py
@@ -14,6 +14,7 @@ from rest_framework_jwt.views import JSONWebTokenAPIView
 
 # Local imports
 from .models import *
+from .utils import generate_jwt_token
 
 
 # API Logic Here
@@ -37,3 +38,30 @@ class TestAppView(APIView):
                 'message': str(e)},
                 status = status.HTTP_400_BAD_REQUEST
             )
+
+
+class RegistrationAPIView(APIView):
+    serializer_class = UserCreateSerializer
+    __doc__ = "Registration API for newly User"
+
+    def post(self, request, *args, **kwargs):
+        try:
+            user_serializer = UserCreateSerializer(data=request.data)
+            if user_serializer.is_valid():
+                user = user_serializer.save()
+                data = generate_jwt_token(user, user_serializer.data)
+                return Response(data, status=status.HTTP_200_OK)
+            else:
+                message = ''
+                for error in user_serializer.errors.values():
+                    message += " "
+                    message += error[0]
+                return Response({
+                    status: False,
+                    'message': message},
+                    status=status.HTTP_400_BAD_REQUEST
+                    )
+        except Exception as e:
+            return Response({'status': False,
+                             'message': str(e)},
+                            status=status.HTTP_400_BAD_REQUEST)
