3612ecbce82c0a7e94ad69eb5670cf072ef1e9cb
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-19 17:37:17 +0530

    views: common project import

--- app/movie/views.py
+++ app/movie/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# python imports
+import requests
+import os
+import datetime
 
-# Create your views here.
+# Django core imports
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.serializers import JSONWebTokenSerializer
+from rest_framework_jwt.views import JSONWebTokenView
+
+# Local imports
+from .models import *
+
+
+# API Logic Here
