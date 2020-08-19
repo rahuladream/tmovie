ba2efb278d8558dcf9e1e2d838ecfd0f31ae8787
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-20 00:47:51 +0530

    user models completed <> Testing Pending

--- app/movie/models.py
+++ app/movie/models.py
@@ -51,3 +51,24 @@ class User(AbstractBaseUser, PermissionMixin, TimestampModel):
     first_name = models.CharField(('First Name'), max_length=30, blank=True)
     last_name = models.CharField(('Last Name'), max_length=30, blank=True)
     role = models.CharField(max_length=20, choices=USER_ROLE)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+    
+    def get_full_name(self):
+        """
+        Return first_name + last_name with space
+        """
+        full_name = '{} {}'.format(self.first_name, self.last_name)
+        return full_name.strip()
+    
+    def get_short_name(self):
+        return self.first_name
+    
+    def __unicode__(self):
+        return self.email
