101ee39c43440041b3a21c4b9a4a0c0eee5152be
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-19 18:11:29 +0530

    adding a sample test api to check the working

--- app/movie/views.py
+++ app/movie/views.py
@@ -10,10 +10,30 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.serializers import JSONWebTokenSerializer
-from rest_framework_jwt.views import JSONWebTokenView
+from rest_framework_jwt.views import JSONWebTokenAPIView
 
 # Local imports
 from .models import *
 
 
 # API Logic Here
+
+class TestAppView(APIView):
+
+    def get(self, request, format=None):
+
+        try:
+            sum = lambda x, y: x + y
+            result = sum(11, 51)
+            
+            return Response({
+                'status': True,
+                'Response': 'On adding {} result is : {}'.format('11 + 51', result) },
+                status = status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response({
+                'status': False,
+                'message': str(e)},
+                status = status.HTTP_400_BAD_REQUEST
+            )
