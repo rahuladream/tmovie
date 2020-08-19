da6ad279613f053c2c3e7ce5743c33f48379418b
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-19 17:49:40 +0530

    additional changes in project base configuration

--- tmovie/settings/base.py
+++ tmovie/settings/base.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'app.movie'
 ]
 
 MIDDLEWARE = [
