54bda186bca363d7880e948a48db80666149e4e1
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-19 17:42:10 +0530

    project and movie app urls fixed

---
+++ app/movie/urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*
+
+from __future__ import print_function, unicode_literals
+# Python Imports
+import logging
+
+# Django Core Imports
+from django.conf.urls import url
+
+# Django rest_framework imports
+
+# Local Imports
+
+app_name = 'movie'
+
+urlpatterns = [
+    # url(r'^test/$',)
+]
