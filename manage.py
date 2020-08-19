ab307a0d39290ea5a8cbd9df5294712c6aabf751
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-19 16:12:17 +0530

    django manage setting according to the need

--- manage.py
+++ manage.py
@@ -3,19 +3,30 @@
 import os
 import sys
 
+from decouple import config
+# Decouple helps you to organize your settings so that you 
+# can change parameters without having to redeploy your app.
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmovie.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+if __name__ == "__main__":
+    settings_module = config("DJANGO_SETTINGS_MODULE", default=None)
+
+    if sys.argv[1] == "test":
+        if settings_module:
+            print(
+                "Ignoring config('DJANGO_SETTINGS_MODULE') because it's test. "
+                "Using 'tmovie.settings.test'"
+            )
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tmovie.settings.test")
+    else:
+        if settings_module is None:
+            print(
+                "Error: no DJANGO_SETTINGS_MODULE found. Will NOT start devserver. "
+                "Remember to create .env file at project root. "
+                "Check README for more info."
+            )
+            sys.exit(1)
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
