50dd0a93b934676f6fe3d3b02a30047a3d1b0cb9
Rahul Singh <rahul.singh4800@gmail.com>
2020-08-19 16:19:05 +0530

    debug mode and local project configuration

--- tmovie/settings/local_base.py
+++ tmovie/settings/local_base.py
@@ -1,11 +1,11 @@
 from .base import *  # noqa
 
 
-DEBUG = True
+DEBUG = config('DEBUG_MODE')
 
 HOST = "http://localhost:8000"
 
-SECRET_KEY = "secret"
+SECRET_KEY = config('SECRET_KEY')
 
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": base_dir_join("db.sqlite3"),}
@@ -21,6 +21,9 @@ DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 AUTH_PASSWORD_VALIDATORS = []  # allow easy passwords only on local
+
+# INSTALLED_APPS
+INSTALLED_APPS += ("",)
 
 # Celery
 CELERY_TASK_ALWAYS_EAGER = True
